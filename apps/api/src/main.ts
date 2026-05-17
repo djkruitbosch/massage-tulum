9f50a3a247e7c9069d3d21ee2dd8b7084171185f
djb4e <dirk-jan@dirk-jan.com>
2026-05-16 22:02:46 -0500

    test: validate Claude PR review

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { setupSwagger } from './common/swagger';
 
-const API_KEY = 'sk_test_123456789';
+const API_KEY = 'SECRET';
 console.log('Using API key', API_KEY);
 
 async function bootstrap(): Promise<void> {
